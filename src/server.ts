b3c2df91c1f0343ad72dfaccff549f076a7f5b14
Your Name <you@example.com>
2026-06-11 10:00:43 -0300

    Continuidade server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,70 @@
-// FALAR SOBRE API REST
+import TodoListClass, { Item } from "./core"
+const todolist = new TodoListClass("todolist.json")
+
+function testRoute(req: Bun.BunRequest) {
+  return Response.json({
+    method: req.method,
+    time: new Date().toLocaleString('pt-BR'),
+    body: req.body?.text(),
+  });
+}
 
 const server = Bun.serve({
-    port: 3000,
-    routes: {
-        "/api-debugger": Bun.file('public/api-debugger.html'),
-        "/test": {
-            GET: () => Response.json({ type: "GET", time: Date.now() }),
-            PUT: () => Response.json({ type: "PUT", time: Date.now() }),
-            POST: () => Response.json({ type: "POST", time: Date.now() }),
-            DELETE: () => Response.json({ type: "DELETE", time: Date.now() }),
-        }
+  port: 3000,
+  routes: {
+    '/api-debugger': (req) => new Response(Bun.file('./public/api-debugger.html')),
+    '/test': {
+      GET: testRoute,
+      POST: testRoute,
+      PUT: testRoute,
+      DELETE: testRoute,
+      PATCH: testRoute,
+      OPTIONS: testRoute,
     },
-    fetch () {
+    '/todo': {
+        GET: async () => {
+             const items = await todolist.getItems()
+            return Response.json(items)
+        },
+        POST: async (req) => {
+            let data
 
-        return new Response("Not Found", { status: 404})
+            try {
+                data = await req.body?.json();
+            } catch (e) {
+                return new Response('Json do corpo mal formatado', { status: 400})
+            }
+            if (!data.title){
+                return new Response ('É preciso informar title', { status: 400})
+
+                try {
+                    await todolist.addItem(new Item(data.title))
+                } catch (error) {
+                    return new Response ('Erro ao adicionar ITEM:', { status: 500})
+                }
+            }
+            return new Response('success', { status: 201 });
+        }
+        
+    },
+    '/todo/:index': {
+        DELETE: (req) => {
+            const indexStr = req.params.index;
+            const index = parseInt(indexStr)
+
+            if(isNaN(index)) {
+                return new Response('Erro, precisa de um número', { status: 400})
+            }
+            const items = todolist.removeItem(index);
+            return new Response(req.params.index)
+        }
     }
-})
 
-console.log(`⚡ http://localhost:${server.port}`)
 
+  },
+  fetch(req) {
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`⚡http://localhost:${server.port}`);
